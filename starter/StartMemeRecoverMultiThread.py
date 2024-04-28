d24bb9df5deff1f8e97651d3071aeb8bc5f26925
Tiago Simoes <tfdsimoes@gmail.com>
2024-04-28 13:24:38 +0200

    Add 2 way to recover images, from total images or until a date.

--- starter/StartMemeRecoverMultiThread.py
+++ starter/StartMemeRecoverMultiThread.py
@@ -1,15 +1,23 @@
 import os
+import psutil
 import requests
+import re
+import sys
 import threading
+
 from bs4 import BeautifulSoup
 
-# Global variables
 base_url = 'https://www.reddit.com'
 community_url = base_url + '/r/SpanishMeme/'
-total_images = 0
-total_images_to_recover = 60
-max_number_processor_images = 6
+
+default_images_to_recover = 70
+total_images_to_recover = None
+last_time_run = None
+
+max_number_processor_images = psutil.cpu_count()
+
 path_images_directory = 'images'
+total_images = 0
 posts = []
 total_images_lock = threading.Lock()
 
@@ -41,6 +49,37 @@ def download_image(image_url, counter):
         image_file.write(image_content)
 
 
+def recover_posts_by_date():
+    page = retrieve_page(community_url)
+    while True:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        posts.extend(soup.find_all('shreddit-post'))
+
+        post_date = posts[0]['created-timestamp']
+
+        if post_date < last_time_run:
+            break
+
+        page = load_more_posts(soup)
+
+
+def recover_posts_by_total_images():
+    page = retrieve_page(community_url)
+    while len(posts) < total_images_to_recover:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        posts.extend(soup.find_all('shreddit-post'))
+
+        page = load_more_posts(soup)
+
+
+def load_more_posts(soup):
+    faceplate_partial_load_posts = soup.select('[id^="partial-more-posts"]')
+    src_value = faceplate_partial_load_posts[0].get('src')
+    load_more_url = base_url + src_value
+    print(f"Next url to load: {load_more_url}")
+    return retrieve_page(load_more_url)
+
+
 def process_posts():
     global total_images
 
@@ -49,28 +88,34 @@ def process_posts():
             number_process = total_images
             total_images = total_images + 1
 
-        print(f"Processing image:  {number_process}")
+        print(f"Processing image with process {number_process}")
         download_image(posts.pop()['content-href'], number_process)
 
 
+# Check if it will process by date or total images
+if len(sys.argv) >= 2:
+    start_argument = sys.argv[1]
+    regex_date = r'\d{4}-\d{2}-\d{2}'
+    if start_argument.isdigit():
+        print(f"Recovering {start_argument} images")
+        total_images_to_recover = int(start_argument)
+        recover_posts_by_total_images()
+    elif re.fullmatch(regex_date, start_argument):
+        print(f"Recovering images from {start_argument}")
+        last_time_run = start_argument
+        recover_posts_by_date()
+else:
+    print(f"Recovering {default_images_to_recover} images by default")
+    total_images_to_recover = default_images_to_recover
+    recover_posts_by_total_images()
+
+# Clean the directory to store the images
 prepare_directory_images(path_images_directory)
 
-# Load initial page
-page = retrieve_page(community_url)
-
-while len(posts) < total_images_to_recover:
-    soup = BeautifulSoup(page.text, 'html.parser')
-    posts.extend(soup.find_all('shreddit-post'))
-
-    faceplate_partial_load_posts = soup.select('[id^="partial-more-posts"]')
-    src_value = faceplate_partial_load_posts[0].get('src')
-    load_more_url = base_url + src_value
-
-    print(f"Next url to load: {load_more_url}")
-    page = retrieve_page(load_more_url)
-
-posts.reverse() # new posts fist
+# Reverse list to process the most recent first
+posts.reverse()
 threads = []
+
 for i in range(max_number_processor_images):
     thread = threading.Thread(target=process_posts)
     thread.start()
